8dad700114b01de7a33f47e5e6294b37ed637044
Waaatanuki <watanukipublic@gmail.com>
2024-08-28 15:40:01 +0800

    feat: 增加卡池角色id信息

--- gbf/gacha/index.ts
+++ gbf/gacha/index.ts
@@ -1,10 +1,19 @@
-import fs from 'node:fs/promises'
+import { readFile, writeFile } from 'node:fs/promises'
+import { load } from 'cheerio'
 import list from './list.json'
 import ratio1 from './ratio1.json'
 import ratio2 from './ratio2.json'
 
 const data = { list, ratio1, ratio2 }
 
-fs.writeFile('./gbf/gacha/gbf_gacha_info.json', JSON.stringify(data)).then(() => {
-  console.log('成功！')
+await writeFile('./gbf/gacha/gbf_gacha_info.json', JSON.stringify(data))
+
+const domStr = await readFile('./gbf/gacha/content.txt', { encoding: 'utf-8' })
+const htmlString = decodeURIComponent(domStr)
+const $ = load(htmlString)
+const characterId: string[] = []
+$(`.img-open-character`).each((i, el) => {
+  characterId.push(el.attribs.alt)
 })
+
+await writeFile('./gbf/gacha/character_list.json', JSON.stringify(characterId))
